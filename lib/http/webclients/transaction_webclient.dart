f556f30a516fd4d655b70da8ff8b6e61aa781c7e
jhoisz <jvrda@ufpi.edu.br>
2022-10-14 10:48:16 -0300

    Aula 03: Evitando Múltiplos problemas

--- lib/http/webclients/transaction_webclient.dart
+++ lib/http/webclients/transaction_webclient.dart
@@ -8,42 +8,38 @@ import '../webclient.dart';
 class TransactionWebClient {
   Future<List<Transaction>?> findAll() async {
     try {
-      final response = await client
-          .get(Uri.parse(baseUrl))
-          .timeout(const Duration(seconds: 5));
+      final response = await client.get(Uri.parse(baseUrl));
+
       final List<dynamic> decodedJson = jsonDecode(response.body);
       return decodedJson
           .map((dynamic json) => Transaction.fromJson(json))
           .toList();
     } catch (e) {
-      // print(e);
       return null;
     }
-    // return null;
-
-    // print(response.body);
   }
 
   Future<Transaction?> save(Transaction transaction, String password) async {
     final String transactionJson = jsonEncode(transaction.toJson());
 
-    final Response response = await client.post(
-      Uri.parse(baseUrl),
-      headers: {
-        'Content-type': 'application/json',
-        'password': password,
-      },
-      body: transactionJson,
-    );
+    final Response response = await client
+        .post(
+          Uri.parse(baseUrl),
+          headers: {
+            'Content-type': 'application/json',
+            'password': password,
+          },
+          body: transactionJson,
+        )
+        .timeout(const Duration(seconds: 3));
 
     if (response.statusCode == 200) {
       return Transaction.fromJson(jsonDecode(response.body));
     }
-    throwHttpError(response.statusCode);
-  }
+    // throwHttpError(response.statusCode);
 
-  void throwHttpError(int statusCode) =>
-      throw Exception(_statusCodeResponses[statusCode]);
+    throw HttpException(_statusCodeResponses[response.statusCode]);
+  }
 
   static final Map<int, String> _statusCodeResponses = {
     400: 'there was an error submitting transaction',
@@ -51,14 +47,7 @@ class TransactionWebClient {
   };
 }
 
-
-  // List<Transaction> _toTransactions(Response response) {
-  //   final List<dynamic> decodedJson = jsonDecode(response.body);
-
-  //   // final List<Transaction> transactions = [];
-
-  //   // for (Map<String, dynamic> transactionJson in decodedJson) {
-  //   //   transactions.add(Transaction.fromJson(transactionJson));
-  //   // }
-  //   return transactions;
-  // }
+class HttpException implements Exception {
+  final String? message;
+  HttpException(this.message);
+}
